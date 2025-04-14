b2707e10e7ee10435cc884e186c9fb84763be327
AngelRagel05 <jimenezragelangel@gmail.com>
2025-04-15 01:47:59 +0200

    Agregar enum Dificultad con descripciones para niveles de dificultad

--- sudoku/src/main/java/model/Dificultad.java
+++ sudoku/src/main/java/model/Dificultad.java
@@ -1,4 +1,18 @@
 package model;
 
 public enum Dificultad {
+
+    FACIL("No sabía que eras un cagado."),
+    MEDIO("Me sorprende que tengas valentía."),
+    DIFICIL("Eres honrado por los Dioses.");
+
+    private final String descripcion;
+
+    private Dificultad (String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
