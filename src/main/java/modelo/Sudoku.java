779b29282b14b68914650303b11fbefd8c764b41
AngelRagel05 <jimenezragelangel@gmail.com>
2025-05-02 11:50:42 +0200

    Agregar validaciones para resolver el Sudoku y mejorar la lógica de verificación

--- src/main/java/modelo/Sudoku.java
+++ src/main/java/modelo/Sudoku.java
@@ -2,8 +2,10 @@ package modelo;
 
 public class Sudoku {
 
+//    Atributos
     private int sudoku[][];
 
+//    Constructor
     public Sudoku() {
         int sudo[][] = {
                 {0, 6, 0, 1, 0, 4, 0, 5, 0},
@@ -19,12 +21,19 @@ public class Sudoku {
         this.sudoku = sudo;
     }
 
+//    Metodo para resolver sudoku
     public void resolverSudoku() {
+
+//        Recorro el tablero
         for (int i = 0; i < this.sudoku.length; i++) {
             for (int j = 0; j < this.sudoku.length; j++) {
+
+//                Si la posición en la que esta es un cero
                 if (this.sudoku[i][j] == 0) {
+
+//                    Valida con los números del 1 al 9 la fila, columna y el subCuadrante
                     for (int valor = 1; valor <= 9; valor++) {
-                        if (validarFila(i, valor)) {
+                        if (validarFila(i, valor) && validarColumna(j, valor) && validarCuadrante(i, j ,valor)) {
 
                         }
                     }
@@ -33,7 +42,28 @@ public class Sudoku {
         }
     }
 
+//    Validar Cuadrante
+    public boolean validarCuadrante(int i, int j, int valor) {
+        int posI = subCuadranteActual(i);
+        int posJ = subCuadranteActual(j);
 
+        for (int k = posI-3; k < posI; k++) {
+            for (int l = posJ-3; l < posJ; l++) {
+                if (sudoku[k][l] == valor) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public int subCuadranteActual (int pos) {
+        if (pos <= 2) return 3;
+        else if (pos <= 5) return 6;
+        else return 9;
+    }
+
+//    Validar fila
     public Boolean validarFila(int i, int valor) {
         for (int j = 0; j < this.sudoku.length; j++) {
             if (sudoku[i][j] == valor) {
@@ -43,6 +73,7 @@ public class Sudoku {
         return true;
     }
 
+//    Validar Columna
     public Boolean validarColumna(int j, int valor) {
         for (int i = 0; i < this.sudoku.length; i++) {
             if (sudoku[i][j] == valor) {
