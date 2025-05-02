fe5031cb69f34145758233fdd1cb66aca6b3cafc
AngelRagel05 <jimenezragelangel@gmail.com>
2025-05-03 00:02:00 +0200

    Agregar método iniciarComponentes en FormaSudoku1 para inicializar el tablero y sus propiedades visuales; actualizar referencia en la clase Ejecutar.

--- src/main/java/interfaz/FormaSudoku1.java
+++ src/main/java/interfaz/FormaSudoku1.java
@@ -5,6 +5,25 @@ import java.awt.*;
 
 public class FormaSudoku1 extends JFrame {
 
+    private TableroSudoku tableroSudoku;
+
+    public void iniciarComponentes() {
+        tableroSudoku = new TableroSudoku();
+        tableroSudoku.setTxtAltura(36);
+        tableroSudoku.setTxtAncho(36);
+        tableroSudoku.setTxtTamañoLetra(27);
+
+        tableroSudoku.setPanelFondo(Color.BLUE);
+        tableroSudoku.setTxtFondo1(Color.BLACK);
+        tableroSudoku.setTxtColorLetra1(Color.BLACK);
+
+        tableroSudoku.setTxtFondo2(Color.BLACK);
+        tableroSudoku.setTxtColorLetra2(Color.WHITE);
+
+        tableroSudoku.setTxtFondo3(Color.BLACK);
+        tableroSudoku.setTxtColorLetra3(Color.WHITE);
+    }
+
     public FormaSudoku1() {
 
 //        Titulo
--- src/main/java/principal/Ejecutar.java
+++ src/main/java/principal/Ejecutar.java
@@ -1,12 +1,13 @@
 package principal;
 
-import interfaz.FormaSudoku;
+import interfaz.FormaSudoku1;
 import modelo.Sudoku;
 
 public class Ejecutar {
 
     public static void main(String[] args) {
-        FormaSudoku sudoku = new FormaSudoku();
+        FormaSudoku1 sudoku = new FormaSudoku1();
+        sudoku.setVisible(true);
 
     }
 }
