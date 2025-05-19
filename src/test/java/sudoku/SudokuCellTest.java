cb0173d8d269ec4d3f0f874ad322a4cd1025cdc7
AngelRagel05 <jimenezragelangel@gmail.com>
2025-05-19 11:08:34 +0200

    Agregar pruebas unitarias para la clase SudokuCell: implementar métodos de prueba para verificar la funcionalidad de celdas fijas y números definitivos.

---
+++ src/test/java/sudoku/SudokuCellTest.java
@@ -0,0 +1,28 @@
+package sudoku;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SudokuCellTest {
+
+    @Test
+    void isFijaUsuario() {
+    }
+
+    @Test
+    void setCeldaFija() {
+    }
+
+    @Test
+    void marcarComoFijaPorUsuario() {
+    }
+
+    @Test
+    void setNumeroDefinitivo() {
+    }
+
+    @Test
+    void getNumeroDefinitivo() {
+    }
+}
